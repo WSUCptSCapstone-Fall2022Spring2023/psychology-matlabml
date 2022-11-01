cb26f766677b06977954a8d0fd63877dd18aa30c
Buggrat <aidan.nunn@wsu.edu>
2022-11-01 16:25:15 -0700

    Minor changes to Print_pl2.py while talking with Charlie about the code.

--- pythonProject/Print_pl2.py
+++ pythonProject/Print_pl2.py
@@ -4,6 +4,9 @@
 # Then, the program iterates through all the available channels to find which ones have data in them (it checks the count variable from pl2_info())
 # Once pl2_ad() gets the data in the form of the named tuple, those values can be printed!
 
+# From my understanding, each a/d number is the LFP at the timeframe when it was recorded. These are recorded over the course of a little over an hour
+# We calculate power at each site, and coherence between sites
+# This means power for each channel (each channel is recorded at a different site) and coherence between channels
 
 import os
 import sys
@@ -14,6 +17,7 @@ from matplotlib import pyplot as plt
 from pypl2 import pl2_info, pl2_ad, pl2_spikes, pl2_events, pl2_info
 
 
+#function that opens a file explorer window to select a file to read
 def choose_file():
     try:
         import tkinter
@@ -61,7 +65,7 @@ if __name__ == "__main__":
     # this loop grabs all channels with counts > 0 and puts them into the list l
     # the channels are 0-indexed in the file, but numbered in repeating sets of 0-32 in the tuple. channelList appends the actual channel each time a good one is found
     for item in resource.ad:
-        if item.n != 0:
+        if item.n > 0:
             l.append(item)
             channelList.append(iterator)
         iterator += 1
@@ -70,32 +74,31 @@ if __name__ == "__main__":
     # usually 8 channels, representing analyses of different parts of the brain
     adList = []
     temp = []
+    index = 0
     for i in channelList:
         ad = pl2_ad(filename, i)
 
         print("Here's what I found in Channel {}".format(i-63))
-        print("Frequency: {}".format(ad.adfrequency)) # this number is how many a/d value are recorded per second (frequency of recordings)
+        print("Frequency: {}".format(ad.adfrequency)) # this number is how many a/d value are recorded per second (frequency of recordings). # There are about 2000 a/d numbers recorded a second for FP channels
         print("Number of Data Points: {}".format(ad.n)) # total number of data points in the channel
         print("List of Timestamps: {}".format(ad.timestamps))
         #print("Number of Fragments: {}".format(ad.fragmentcounts))
-        for j in range(0, 256):  #len(ad.ad)): there are millions of data points in this tuple, so viewing the first 100 is more feasible for this test
+        for j in range(256):# len(ad.ad)): #there are millions of data points in this tuple, so viewing the first 100 or so is more feasible for this test
             temp.append(ad.ad[j])
             print("a/d Number {}: {}".format(j, ad.ad[j]))
         adList.append(temp)
         temp = []
         print('\n')
 
-    #prints two arrays, one of frequencies and one of PSDs. From my understanding these will be used to create a graph.
-    f, Pxx = welch(adList[0])
-    print("Array of sample frequencies: {}".format(f))
-    print("Power spectral density or power spectrum of input array: {}".format(Pxx))
-    plt.plot(f, Pxx)
-    plt.xlabel("Frequency")
-    plt.ylabel("PSD")
-    plt.show()
+        # try using scipy.signal.welch as a replacement for MATLAB's pwelch function
+        # prints two arrays, one of frequencies and one of PSDs. From my understanding these will be used to create a graph.
+        f, Pxx = welch(adList[index])
+        print("Array of sample frequencies: {}".format(f))
+        print("Power spectral density or power spectrum of input array: {}".format(Pxx))
+        plt.plot(f, Pxx)# plot a graph of frequencies on the x-axis and PSDs on the y-axis
+        plt.xlabel("Frequency")
+        plt.ylabel("PSD")
+        plt.show()# open window displaying graph
+        index += 1
 
-# From my understanding, each a/d number is the LFP at the timeframe when it was recorded. These are recorded over the course of a little over an hour
-# There are about 2000 a/d numbers recorded a second
-# We calculate power at each site, and coherence between sites
-# This means power for each channel (each channel is recorded at a different site) and coherence between channels
-# try using scipy.signal.welch as a replacement for MATLAB's pwelch function
+
