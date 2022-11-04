984bba12e52810675b14099c334fe33dda43c01d
Buggrat <aidan.nunn@wsu.edu>
2022-11-03 19:57:26 -0700

    Added lots of comments to make code more readable

--- pythonProject/A-D Lasso Predictor.py
+++ pythonProject/A-D Lasso Predictor.py
@@ -1,27 +1,29 @@
-# This is another practice file where I will prove a concept for a lasso predictor First, import all data from sample
-# files into the program and sort them into two arrays by A and D types Then, for each file, calculate the power for
-# each channel in that file and store in a dictionary of arrays Do the same for coherence for each file,
-# using a dictionary of dictionaries of arrays? to store coherence between each channel The power and coherence
-# values are arrays. Hopefully this still works for the sklearn lasso function Place all of this data into a pandas
-# dataframe in the following format (arrays are replaced with random chars for convenience of the example):
+# This is another practice file where I will prove a concept for a lasso predictor. First, import all data from sample
+# files into the program and sort them into two arrays by A and D types. Then, for each file, calculate the power for
+# each channel in that file and store in a dictionary of arrays. Do the same for coherence for each file.
+# The power and coherence values are in arrays, but to avoid an error with numpy we take the mean of the array for now.
+# Place all of this data into a pandas dataframe in the following format:
 #
-# |   File Name   |   Channel 1 Power    |   Channel 2 Power   | ... |   Channel 8 Power   |   Coherence Between 1 and 2   |   Coherence Between 1 and 3   | ... | Coherence Between 7 and 8   |   A or D   |
-#     A170...                [xyz]                [zyx]                        [xyz]                       [yxz]                   [zyw]                                     [yzs]                    1
+# |   Channel 1 Power    |   Channel 2 Power   | ... |   Channel 8 Power   |   Coherence Between 1 and 2   |   Coherence Between 1 and 3   | ... | Coherence Between 7 and 8   |   A or D   |
+#             ####                ####                       ####                       #####                       ####                                      ######                   1
 #
+# The rows will be indexed by file name.
 # We use 1 to describe A and 0 to describe D in the dataframe
 # Use 80% of the data as training data, keeping 20% as testing Fit the lasso model by using the channel powers and
-# coherences as features, and the 'A or D' column as the target We can also use a series of lambda values to test
-# multiple times to find the best fit Use all of the data to prevent overfitting. We can use os.chdir() to find the
+# coherences as features, and the 'A or D' column as the target. We can also use a series of lambda values to test
+# multiple times to find the best fit. Use all of the data to prevent overfitting. We can use os.chdir() to find the
 # directory where the data is stored and run on all of it at once
 
 # Additionally, this file does not represent our final software product. As stated above, it is a proof of concept
 # for Dr. Henrick's initial assignment of creating an A/D binary classifier
 
+# Import Statements
 import os
 import sys
 import warnings
 import ctypes
 import numpy as np
+import gc
 from statistics import mean
 from scipy import signal
 from sklearn.linear_model import Lasso
@@ -39,20 +41,27 @@ def divide_chunks(l, n):
         yield l[i:i + n]
 
 
+# start of the program
 if __name__ == "__main__":
 
     # add all data file names in the target directory to a list
-    sDir = r'C:\Users\aidan.nunn\Documents\Homework\CS 421\SampleSampleData'
-    fType = '.pl2'
-    os.chdir(
-        sDir)  # set the directory we are looking at to where all of the data is stored. Since this is a proof of concept, this will be different for each machine this program runs on.
-    files = []
+
+    sDir = r'C:\Users\aidan.nunn\Documents\Homework\CS 421\SampleSampleData'  # Target directory for Aidan's machine.
+    # This string needs to be changed for other users.
+
+    fType = '.pl2'  # File type we are looking for
+
+    os.chdir(sDir)  # change the current working directory to where our data is stored.
+    files = []  # make a list to store file names
     print("Finding file names")
+
+    # For each file in a list of files in the directory
     for f in os.listdir(sDir):
-        if f.endswith(fType):
-            files.append(f)
+        if f.endswith(fType):  # if the file ends with our target file type
+            files.append(f)  # append it to our list of file names
 
     # Create a pandas dataframe for use later
+    # Rows are indexed by file name, and columns are labeled with each data point we want to store.
     df = pd.DataFrame(index=files, columns=['Channel 1 Power', 'Channel 2 Power', 'Channel 3 Power', 'Channel 4 Power',
                                             'Channel 5 Power', 'Channel 6 Power', 'Channel 7 Power', 'Channel 8 Power',
 
@@ -65,30 +74,39 @@ if __name__ == "__main__":
                                             'Coherence 5 & 8', 'Coherence 6 & 7', 'Coherence 6 & 8', 'Coherence 7 & 8',
 
                                             'A or D'])
-    # Get generic file data from each file into a named resource tuple and put each resource object into an array
+
+    # Get generic file data from each file into a named tuple and put each tuple into an array
     resourceArray = []
     print("Getting generic data")
     for filename in files:
         resource = pl2_info(filename)
         resourceArray.append(resource)
 
-    # Get AD file data from each file and put each resource object into a dictionary
-    ADict = {}
-    DDict = {}
-    channelsList = []
+    # Get A/D file data from each file and put each tuple into a dictionary
+    # We will store A group data in one dictionary and D group data in another
+    ADict = {}  # Dictionary for A group data
+    DDict = {}  # Dictionary for D group data
+    channelsList = []  # List to hold 0-indexed channel numbers instead of what is listed in the resource tuples
     print("Getting AD data")
+
+    # zip files and resourceArray together so we can iterate through them at the same time
     for filename, resource in zip(files, resourceArray):
         channelNameIterator = 0  # reset iterator between files
-        for channel in resource.ad:
-            if channel.n > 0:
-                ad = pl2_ad(filename, channelNameIterator)  # add channel variable
-                if filename.startswith('A'):
+        for channel in resource.ad:  # iterate over each channel in a file (8 relevant channels per file)
+            if channel.n > 0:  # if a channel has count > 0 then it has data
+                ad = pl2_ad(filename, channelNameIterator)  # use pl2_ad to get a/d info from a channel
+                if filename.startswith('A'):  # the filename starts with 'A' if it's in the A group
                     ADict[(filename, channelNameIterator)] = ad  # keys are tuples named as (filename, channel number)
-                    channelsList.append(channelNameIterator)
-                if filename.startswith('D'):
+                    channelsList.append(channelNameIterator)  # add the channel number to a list for use later
+                if filename.startswith('D'):  # the filename starts with 'D' if it's in the D group
                     DDict[(filename, channelNameIterator)] = ad  # keys are tuples named as (filename, channel number)
-                    channelsList.append(channelNameIterator)
-            channelNameIterator += 1
+                    channelsList.append(channelNameIterator)  # add the channel number to a list for use later
+            channelNameIterator += 1  # iterate to the next 0-indexed channel number
+
+    del files  # Release memory holding all file names
+    gc.collect()
+    del resourceArray  # Release memory holding all files generic info
+    gc.collect()
 
     # Lists of channel names to be used in the below loops
     dfPowerChannelNames = ['Channel 1 Power', 'Channel 2 Power', 'Channel 3 Power', 'Channel 4 Power',
@@ -107,9 +125,12 @@ if __name__ == "__main__":
     print("Calculating Power for A groups and storing in the dataframe")
     channelNameIterator = 0
     for key in ADict:
-        df.at[key[0], 'A or D'] = 1
-        f, Pxx = signal.welch(ADict[key].ad)
+        df.at[key[0], 'A or D'] = 1  # set the 'A or D' column value to 1 to represent it being an A column
+        f, Pxx = signal.welch(ADict[key].ad)  # Use welch() to calculate the power array from the list of LFPs in ADict
+        # use the average of power values because of an error with numpy that needs to be resolved
         df.at[key[0], dfPowerChannelNames[channelNameIterator]] = mean(Pxx)
+        # This if/else check makes sure the channelNameIterator doesn't go past 7, since the list dfPowerChannelNames
+        # is only 8 values long (0-indexed)
         if channelNameIterator < 7:
             channelNameIterator += 1
         else:
@@ -121,9 +142,12 @@ if __name__ == "__main__":
     print("Calculating Power for D groups and storing in the dataframe")
     channelNameIterator = 0
     for key in DDict:
-        df.at[key[0], 'A or D'] = 0
-        f, Pxx = signal.welch(DDict[key].ad)
+        df.at[key[0], 'A or D'] = 0  # set the 'A or D' column value to 0 to represent it being a D column
+        f, Pxx = signal.welch(DDict[key].ad)  # Use welch() to calculate the power array from the list of LFPs in DDict
+        # use the average of power values because of an error with numpy that needs to be resolved
         df.at[key[0], dfPowerChannelNames[channelNameIterator]] = mean(Pxx)
+        # This if/else check makes sure the channelNameIterator doesn't go past 7, since the list dfPowerChannelNames
+        # is only 8 values long (0-indexed)
         if channelNameIterator < 7:
             channelNameIterator += 1
         else:
@@ -134,12 +158,25 @@ if __name__ == "__main__":
     # We need combinations between each chunk of 8 keys in the dictionary
     print("Calculating Coherence for A groups and storing in the dataframe")
     channelNameIterator = 0
+
+    # divide the list of dictionary keys into chunks of 8 because each file has 8 channels
+    # this enables us to calculate combinations between each file's channels
     divList = list(divide_chunks(list(ADict.keys()), 8))
+
+    # each sublist in divList represents a file, hence the naming convention
     for file in divList:
-        combList = list(combinations(file, 2))
+        combList = list(combinations(file, 2))  # get all combinations of the channels in the file
+
+        # iterate through the combinations
         for comb in combList:
+            # calculate the coherence between two channels
+            # comb is a nested tuple of channel names
             f, Cxy = signal.coherence(ADict[comb[0]].ad, ADict[comb[1]].ad)
+
+            # put the mean of the coherence in the right dataframe cell
             df.at[comb[0][0], dfCoherenceChannelNames[channelNameIterator]] = mean(Cxy)
+
+            # 28 coherence columns
             if channelNameIterator < 27:
                 channelNameIterator += 1
             else:
@@ -150,20 +187,40 @@ if __name__ == "__main__":
     # We need combinations between each chunk of 8 keys in the dictionary
     print("Calculating Coherence for D groups and storing in the dataframe")
     channelNameIterator = 0
+
+    # divide the list of dictionary keys into chunks of 8 because each file has 8 channels
+    # this enables us to calculate combinations between each file's channels
     divList = list(divide_chunks(list(DDict.keys()), 8))
+
+    # each sublist in divList represents a file, hence the naming convention
     for file in divList:
-        combList = list(combinations(file, 2))
+        combList = list(combinations(file, 2))  # get all combinations of the channels in the file
+
+        # iterate through the combinations
         for comb in combList:
+            # calculate the coherence between two channels
+            # comb is a nested tuple of channel names
             f, Cxy = signal.coherence(DDict[comb[0]].ad, DDict[comb[1]].ad)
+
+            # put the mean of the coherence in the right dataframe cell
             df.at[comb[0][0], dfCoherenceChannelNames[channelNameIterator]] = mean(Cxy)
+
+            # 28 coherence columns
             if channelNameIterator < 27:
                 channelNameIterator += 1
             else:
                 channelNameIterator = 0
 
-    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        #print(df)
+    del ADict  # Release dictionary holding A group info
+    gc.collect()
+    del DDict  # Release dictionary holding D group info
+    gc.collect()
 
+    # print the dataframe in its entirety. Currently commented out to save space in the terminal
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        # print(df)
+
+    # lists holding the target value we want to predict and the feature values we will use in the prediction
     target = ['A or D']
     features = ['Channel 1 Power', 'Channel 2 Power', 'Channel 3 Power', 'Channel 4 Power',
                 'Channel 5 Power', 'Channel 6 Power', 'Channel 7 Power', 'Channel 8 Power',
@@ -175,22 +232,21 @@ if __name__ == "__main__":
                 'Coherence 4 & 7', 'Coherence 4 & 8', 'Coherence 5 & 6', 'Coherence 5 & 7',
                 'Coherence 5 & 8', 'Coherence 6 & 7', 'Coherence 6 & 8', 'Coherence 7 & 8']
 
+    # set y as the target column and x as the features columns
     y = df[target]
     x = df[features]
 
-    #x = np.array(x.explode()).reshape(len(x), -1)
-
     # split the data into test and train sets
+    # test_size=0.2 makes it so 80% of data is for training and 20% is for testing
     print("Splitting data")
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
 
     print("Fitting Data")
-    lambda_val = 0.1
-    lasso = Lasso(lambda_val)
-    lasso.fit(x_train, y_train)
-    y_pred = lasso.predict(x_test)
-    mse_lasso = mean_squared_error(y_pred, y_test)
+    lambda_val = 0.1  # set lambda. review documentation for explanation on what this does
+    lasso = Lasso(lambda_val)  # create lasso model
+    lasso.fit(x_train, y_train)  # fit lasso model to our training data
+    y_pred = lasso.predict(x_test)  # make a prediction
+    mse_lasso = mean_squared_error(y_pred, y_test)  # calculate the mean squared error of the prediction
     print(("\nLasso MSE with Lambda={} is {}").format(lambda_val, mse_lasso))
-
 
     print("\ndone")
