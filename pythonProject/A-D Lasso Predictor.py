997e856af31db8550627eb055ea670be5ae3a20c
Buggrat <aidan.nunn@wsu.edu>
2022-11-18 15:21:34 -0800

    Made some changes to the AD predictor file to use the updated preprocessing module.

--- pythonProject/A-D Lasso Predictor.py
+++ pythonProject/A-D Lasso Predictor.py
@@ -21,17 +21,18 @@
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from External_Data_Access_and_Preprocessing_Module import AccessData
+from External_Data_Access_and_Preprocessing_Module import AccessData, LoadData
 
 
 # start of the program
 if __name__ == "__main__":
 
-    dataObject = AccessData(r'C:\Users\aidan.nunn\Documents\Homework\CS 421\Sample Data')
+    processingObject = AccessData(r'C:\Users\aidan.nunn\Documents\Homework\CS 421\Sample Data')
 
     # lists holding the target value we want to predict and the feature values we will use in the prediction
+    features = processingObject.header
     target = ['A or D']
-    features = ['Channel 1 Power', 'Channel 2 Power', 'Channel 3 Power', 'Channel 4 Power',
+    """features = ['Channel 1 Power', 'Channel 2 Power', 'Channel 3 Power', 'Channel 4 Power',
                 'Channel 5 Power', 'Channel 6 Power', 'Channel 7 Power', 'Channel 8 Power',
                 'Coherence 1 & 2', 'Coherence 1 & 3', 'Coherence 1 & 4', 'Coherence 1 & 5',
                 'Coherence 1 & 6', 'Coherence 1 & 7', 'Coherence 1 & 8', 'Coherence 2 & 3',
@@ -39,7 +40,9 @@ if __name__ == "__main__":
                 'Coherence 2 & 8', 'Coherence 3 & 4', 'Coherence 3 & 5', 'Coherence 3 & 6',
                 'Coherence 3 & 7', 'Coherence 3 & 8', 'Coherence 4 & 5', 'Coherence 4 & 6',
                 'Coherence 4 & 7', 'Coherence 4 & 8', 'Coherence 5 & 6', 'Coherence 5 & 7',
-                'Coherence 5 & 8', 'Coherence 6 & 7', 'Coherence 6 & 8', 'Coherence 7 & 8']
+                'Coherence 5 & 8', 'Coherence 6 & 7', 'Coherence 6 & 8', 'Coherence 7 & 8']"""
+
+    dataObject = LoadData(r'C:\Users\aidan.nunn\Documents\Homework\CS 421\Sample Data\test.csv')
 
     # set y as the target column and x as the features columns
     y = dataObject.df[target]
