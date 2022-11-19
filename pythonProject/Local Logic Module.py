2099d6b9a9126b24b57915beeffe5ed249c81e0a
charlie-nickerson <charlie@nickerson.com>
2022-11-18 17:10:58 -0800

    Created the local logic module class
    Added a method for predicting the lasso model, and testing the accuracy

--- pythonProject/Local Logic Module.py
+++ pythonProject/Local Logic Module.py
@@ -3,4 +3,17 @@ This file contains the class which stores algorithms that will be run locally.
 It will have one class, LocalLogic, which holds the algorithm for testing the accuracy of a machine learning model and running the model against experiment data to make predictions on the data.
 """
 
+from sklearn.linear_model import Lasso
+from sklearn.metrics import accuracy_score
+
 class LocalLogic:
+
+    def __init__(self, val, model):
+        self.val = val
+        self.model = Lasso(val)
+
+    def Predict(self, x_test):
+        return self.model.predict(x_test) 
+
+    def TestAccuracy(y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
