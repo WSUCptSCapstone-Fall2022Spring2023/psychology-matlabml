2f67222175acb669f03e1ff5cfed8421ab1e0ff4
charlie-nickerson <charlie@nickerson.com>
2022-11-28 20:33:58 -0800

    Added Fit and Mean Squared Error Functions

--- pythonProject/Local Logic Module.py
+++ pythonProject/Local Logic Module.py
@@ -5,12 +5,19 @@ It will have one class, LocalLogic, which holds the algorithm for testing the ac
 
 from sklearn.linear_model import Lasso
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 
 class LocalLogic:
 
     def __init__(self, val, model):
         self.val = val
         self.model = Lasso(val)
+
+    def Fit(X, y):
+        return self.model.fit(X, y)
+
+    def MeanSquaredError():
+        return 
 
     def Predict(self, x_test):
         return self.model.predict(x_test) 
