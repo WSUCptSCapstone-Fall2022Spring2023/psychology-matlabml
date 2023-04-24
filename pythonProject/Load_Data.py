77ae80e81c05f0080296f6d15a74681e4d8d31d9
charlie-nickerson <charlie@nickerson.com>
2023-04-23 21:07:33 -0700

    Added methods for saving and loading model

--- pythonProject/Load_Data.py
+++ pythonProject/Load_Data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 
 class LoadData:
     """Class used to load a CSV file of data into a pandas dataframe for use in the logic modules"""
@@ -10,3 +11,9 @@ class LoadData:
         """Method for printing the dataframe to a terminal window."""
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             print(self.df)
+
+    def saveModel(model, filename):
+        pickle.dump(model, open(filename, 'wb'))
+
+    def loadModel(filename):
+        return pickle.load(open(filename, 'rb'))
